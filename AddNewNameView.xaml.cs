53278c560dd49d5d0e4fbc3387ed475e0f31ebd1
Dat Nguyen <66193085+ntdat024@users.noreply.github.com>
2024-09-04 11:18:22 +0700

    Add files via upload

---
+++ AddNewNameView.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace NDT_RevitAPI
+{
+    /// <summary>
+    /// Interaction logic for AddNewNameView.xaml
+    /// </summary>
+    public partial class AddNewNameView : Window
+    {
+        private string SPECIAL_CHARS = @"\:{}[];<>?`~";
+        public string NewName { get; set; }
+        public AddNewNameView()
+        {
+            InitializeComponent();
+            tb_SetName.Focus();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            string input = tb_SetName.Text;
+            if (CheckInput(input))
+            {
+                NewName = input;
+                DialogResult = true;
+            }
+            else tb_SetName.Focus();
+        }
+
+        private bool CheckInput(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                MessageBox.Show($"Name cannot empty!", "Message");
+                return false;
+            }
+
+            var array = SPECIAL_CHARS.ToCharArray();
+            foreach (char c in array)
+            {
+                if (input.ToCharArray().Contains(c))
+                {
+                    MessageBox.Show($"Name cannot contain any of special characters:\n{SPECIAL_CHARS}", "Message");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ CreatePrintSetCmd.cs
@@ -0,0 +1,78 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using DatNguyenTool;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace NDT_RevitAPI
+{
+    [Transaction(TransactionMode.Manual)]
+    internal class CreatePrintSetCmd : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            //select sheets in project browser
+            ICollection<ElementId> ids = uidoc.Selection.GetElementIds();
+
+            var sheets = new List<ViewSheet>();
+            foreach(ElementId id in ids)
+            {
+                Element ele = doc.GetElement(id);
+                if (ele is ViewSheet vs) sheets.Add(vs);
+            }
+
+            PrintManager pM = doc.PrintManager;
+            List<string> sheetSetNames = SheetUtils.GetViewSetName(doc);
+
+            if (sheets.Count > 0)
+            {
+
+                var window = new AddNewNameView();
+                window.ShowDialog();
+
+                if (window.DialogResult == true)
+                {
+                    string setName = window.NewName;
+                    if (!sheetSetNames.Contains(setName))
+                    {
+                        using (Transaction t = new Transaction(doc, "CreatePrintSet"))
+                        {
+                            t.Start();
+
+                            ViewSet myViewSet = new ViewSet();
+                            foreach (ViewSheet view in sheets)
+                            {
+                                myViewSet.Insert(view);
+                            }
+
+                            pM.PrintRange = PrintRange.Select;
+
+                            ViewSheetSetting viewSheetSetting = pM.ViewSheetSetting;
+                            viewSheetSetting.CurrentViewSheetSet.Views = myViewSet;
+                            viewSheetSetting.SaveAs(setName);
+
+                            t.Commit();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Name is already in use.\nEnter a unique name.", "Message");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("0 sheets selected!", "Message");
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
